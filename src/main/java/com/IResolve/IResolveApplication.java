3bee55fd1f88cd24b6d17488a26fc6475bac319d
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-11-11 11:13:23 +0530

    Initializing project and adding dependencies.

---
+++ src/main/java/com/IResolve/IResolveApplication.java
@@ -0,0 +1,13 @@
+package com.IResolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IResolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IResolveApplication.class, args);
+	}
+
+}
