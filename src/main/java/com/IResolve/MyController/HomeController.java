739799beafd9ad7d20a8870402ec9238d296fb44
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-11-11 11:17:13 +0530

    Adding about page

--- src/main/java/com/IResolve/MyController/HomeController.java
+++ src/main/java/com/IResolve/MyController/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String Home(){
         return "index";
     }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
 }
